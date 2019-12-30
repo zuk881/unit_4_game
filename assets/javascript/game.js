e869370f19e97a14d2da3b08ded84f9acc0e1aac
zuk881 <zuk881@cox.net>
2019-12-29 20:49:38 -0700

    comments to code added plus styling tweaks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    // var RndInt = 0;
+    // global variables needed for game 
     var btn1;
     var btn2;
     var btn3;
@@ -9,25 +9,22 @@ $(document).ready(function () {
     var wins = 1;
     var losses = 1;
 
-
+    // only random number generator needed for game 
     function getRndInteger(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+
+    // function call for main number generation and JQuery to send to proper div
     var newRndInt = getRndInteger(19, 120)
-    $("#rndInt").html(newRndInt)
+    $("#rndInt").html(newRndInt);
 
-    var btn1 = getRndInteger(1, 12)
-    console.log(btn1);
+    // random number function call for 4 crystal buttons
+    var btn1 = getRndInteger(1, 12);
+    var btn2 = getRndInteger(1, 12);
+    var btn3 = getRndInteger(1, 12);
+    var btn4 = getRndInteger(1, 12);
 
-    var btn2 = getRndInteger(1, 12)
-    console.log(btn2);
-
-    var btn3 = getRndInteger(1, 12)
-    console.log(btn3);
-
-    var btn4 = getRndInteger(1, 12)
-    console.log(btn4);
-
+    // function to reset game if conditionals are met 
     function reset() {
         newRndInt = getRndInteger(19, 120);
         $("#rndInt").html(newRndInt);
@@ -38,7 +35,8 @@ $(document).ready(function () {
         playerTotal = 0;
         $("#total-score").html("Total score:");
     }
-
+    
+    // button functions, total score calculation, and methods to send info to proper div
     $("#btn-1").click(function () {
         playerTotal = playerTotal + btn1
         $("#total-score").html("Total score: " + playerTotal)
@@ -62,7 +60,8 @@ $(document).ready(function () {
         $("#total-score").html("Total score: " + playerTotal)
         $("#winLoss").fadeIn(10000).html("Win-Lose?");
     });
-
+    
+    // on.click function, conditionals, display messages, win/loss tracking, and call reset  
     $(".number").click(function () {
         if (playerTotal === newRndInt) {
             $("#winLoss").html("You Win!!!");
